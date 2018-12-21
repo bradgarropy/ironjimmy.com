deaa1f8576fca1936ea7f58d885b0c62c14f62d4
Brad Garropy <bradgarropy@gmail.com>
2018-12-20 21:59:15 -0600

    removing pwa plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,21 +8,6 @@ module.exports = {
             resolve: "gatsby-plugin-react-helmet",
         },
         {
-            resolve: "gatsby-plugin-manifest",
-            options: {
-                name: "gatsby starter",
-                short_name: "gatsby starter",
-                start_url: "/",
-                background_color: "#ffffff",
-                theme_color: "#ffffff",
-                display: "minimal-ui",
-                icon: "static/icons/gatsby-icon.png",
-            },
-        },
-        {
-            resolve: "gatsby-plugin-offline",
-        },
-        {
             resolve: "gatsby-plugin-google-analytics",
             options: {
                 trackingId: process.env.TRACKING_ID,
