4c5e1bc07de6d24ee14b736f9b1bcb33cf71d22b
Brad Garropy <bradgarropy@gmail.com>
2018-12-25 22:39:39 -0600

    contact and history pages.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,48 @@
 import React from "react"
+import PropTypes from "prop-types"
+import {graphql} from "gatsby"
+import Markdown from "markdown-to-jsx"
 import Layout from "../components/Layout"
 
-const Contact = () => {
-    return <Layout>Contact</Layout>
+const Contact = ({data}) => {
+    const history = data.allContentfulContact.edges[0].node
+    const {description} = history.description
+
+    return (
+        <Layout>
+            <div className="contact">
+                <Markdown>{description}</Markdown>
+                <form>
+                    <label>Name</label>
+                    <input type="text"/>
+                    <label>Email</label>
+                    <input type="email"/>
+                    <label>Message</label>
+                    <textarea/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        </Layout>
+    )
 }
+
+Contact.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+export const query = graphql`
+    {
+        allContentfulContact {
+            edges {
+                node {
+                    description {
+                        description
+                    }
+                }
+            }
+        }
+    }
+`
 
 // export
 export default Contact
--- src/pages/history.js
+++ src/pages/history.js
@@ -1,9 +1,39 @@
 import React from "react"
+import PropTypes from "prop-types"
+import {graphql} from "gatsby"
+import Markdown from "markdown-to-jsx"
 import Layout from "../components/Layout"
 
-const History = () => {
-    return <Layout>History</Layout>
+const History = ({data}) => {
+    const history = data.allContentfulHistory.edges[0].node
+    const {description} = history.description
+
+    return (
+        <Layout>
+            <div className="history">
+                <Markdown>{description}</Markdown>
+            </div>
+        </Layout>
+    )
 }
+
+History.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+export const query = graphql`
+    {
+        allContentfulHistory {
+            edges {
+                node {
+                    description {
+                        description
+                    }
+                }
+            }
+        }
+    }
+`
 
 // export
 export default History
