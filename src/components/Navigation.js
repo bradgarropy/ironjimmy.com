56314a2064c9097b65d151dba2a2ee5c8fb882ad
Brad Garropy <bradgarropy@gmail.com>
2019-01-02 23:11:40 -0600

    updated header styles.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,10 +5,8 @@ import "../scss/Navigation.scss"
 const Navigation = () => {
     return (
         <nav className="navigation">
-            <Link to="/sleeves">Sleeves</Link>
-            <Link to="/straps">Straps</Link>
-            <Link to="/apparel">Apparel</Link>
-            <Link to="/contact">Contact</Link>
+            <Link to="/sleeves">Sleeves</Link> |
+            <Link to="/contact">Contact</Link> |
             <Link to="/history">Our History</Link>
         </nav>
     )
