1ab2ecaae55c2415ea257d32fd152fa4cd0b9282
Brad Garropy <bradgarropy@gmail.com>
2019-01-22 21:45:10 -0600

    hoodie and tshirt content.

--- src/pages/apparel.js
+++ src/pages/apparel.js
@@ -4,37 +4,40 @@ import Container from "../styles/Container"
 import ProductCategoryHeader from "../styles/ProductCategoryHeader"
 import ProductGrid from "../styles/ProductGrid"
 import ProductPreview from "../styles/ProductPreview"
+import {displayPrice} from "../utils/price"
 
 const Straps = () => {
-    const image =
-        "https://downloads.ctfassets.net/d3ttfid6hh7h/2LLl2zMhF6mqm80EAaOGms/24a854839313a91a6d5894febf432015/DSC04650.jpg"
-
     return (
         <Layout>
             <Container>
                 <ProductCategoryHeader>
                     <h1>Apparel</h1>
+                    <img src="https://downloads.ctfassets.net/d3ttfid6hh7h/5elMgAtAQijXnaxhF8nJaG/03253488203c87a0237f5944c90adde4/DSC04623.jpg"/>
                 </ProductCategoryHeader>
 
                 <ProductGrid columns="2">
                     <ProductPreview>
                         <a href="/apparel/tshirt">
-                            <img src={image}/>
+                            <img src="https://downloads.ctfassets.net/d3ttfid6hh7h/2LLl2zMhF6mqm80EAaOGms/24a854839313a91a6d5894febf432015/DSC04650.jpg"/>
                         </a>
 
                         <a href="/apparel/tshirt">
                             <h2>T-Shirt</h2>
                         </a>
+
+                        <p>{displayPrice(20)}</p>
                     </ProductPreview>
 
                     <ProductPreview>
                         <a href="/apparel/hoodie">
-                            <img src={image}/>
+                            <img src="https://images.ctfassets.net/d3ttfid6hh7h/6LV92LcOGo3cfCvDctgvxY/b9e846dbf79d368a2f1cb91fe635f375/DSC04643.jpg"/>
                         </a>
 
                         <a href="/apparel/hoodie">
                             <h2>Hoodie</h2>
                         </a>
+
+                        <p>{displayPrice(40)}</p>
                     </ProductPreview>
                 </ProductGrid>
             </Container>
---
+++ src/pages/apparel/hoodie.js
@@ -0,0 +1,106 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {graphql} from "gatsby"
+import styled from "styled-components"
+import Layout from "../../components/Layout"
+import Container from "../../styles/Container"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faCartPlus} from "@fortawesome/free-solid-svg-icons"
+import {displayPrice} from "../../utils/price"
+
+const Product = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 5rem;
+`
+
+const Title = styled.h1`
+    margin: 0 0 2rem 0;
+`
+
+const Form = styled.form`
+    display: grid;
+    row-gap: 2rem;
+    margin: 3rem 0;
+`
+
+const Field = styled.div`
+    display: grid;
+`
+
+const Colors = styled.div`
+    margin: 3rem 0;
+`
+
+const ColorGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, auto);
+    column-gap: 2rem;
+    row-gap: 2rem;
+    justify-content: start;
+`
+
+const Color = styled.div`
+    width: 5rem;
+    height: 5rem;
+    background: ${props => props.color};
+`
+
+const Leather = ({data}) => {
+    console.log(data)
+
+    const image =
+        "https://images.ctfassets.net/d3ttfid6hh7h/6LV92LcOGo3cfCvDctgvxY/b9e846dbf79d368a2f1cb91fe635f375/DSC04643.jpg"
+    const description = "Cozy comfy hoodie with our logo on it."
+    const price = 40
+
+    return (
+        <Layout>
+            <Container>
+                <Product>
+                    <div>
+                        <img src={image}/>
+                        <p>{description}</p>
+                    </div>
+
+                    <div>
+                        <Title>Hoodie</Title>
+                        <p>{displayPrice(price)}</p>
+
+                        <Form>
+                            <button>
+                                <FontAwesomeIcon icon={faCartPlus}/> Add To
+                                Cart
+                            </button>
+                        </Form>
+                    </div>
+                </Product>
+            </Container>
+        </Layout>
+    )
+}
+
+Leather.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+export const query = graphql`
+    {
+        allContentfulSleeves {
+            edges {
+                node {
+                    contentful_id
+                    image {
+                        file {
+                            url
+                        }
+                    }
+                    name
+                    price
+                }
+            }
+        }
+    }
+`
+
+export default Leather
