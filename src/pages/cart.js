8ffcb8f893a1711a1d5a9fa6f0a417b7b1dde892
Brad Garropy <bradgarropy@gmail.com>
2019-03-03 23:12:50 -0600

    cart styles. closes #14.

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,26 +1,63 @@
 import React, {useContext} from "react"
 import styled from "styled-components"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faTimesCircle} from "@fortawesome/free-solid-svg-icons"
 import CartContext from "../context/CartContext"
+import {displayPrice} from "../utils/price"
+import Container from "../styles/Container"
 import colors from "../styles/colors"
 
 const StyledCart = styled.div`
     display: grid;
-    text-align: center;
+    row-gap: 5rem;
+
+    h1 {
+        margin: 0 0 2rem 0;
+        text-align: center;
+        text-transform: uppercase;
+    }
+`
+
+const CartLineItems = styled.div`
+    display: grid;
+    row-gap: 5rem;
 `
 
 const CartLineItem = styled.div`
     display: grid;
-    grid-auto-flow: column;
+    grid-template-columns: auto 70fr 40fr auto;
+    column-gap: 5rem;
+    align-items: start;
+    justify-items: center;
 `
 
-const CartItemImage = styled.img`
+const CartLineItemImage = styled.img`
     width: 6rem;
 `
 
-const Remove = styled.button`
-    padding: 1rem;
-    border: 3px solid black;
-    background-color: inherit;
+const CartLineItemTitle = styled.p`
+    font-size: 2.5rem;
+    font-weight: 600;
+    margin: 0 0 0.5rem 0;
+`
+
+const CartLineItemVariant = styled.p`
+    margin: 0;
+    color: ${colors.grey};
+`
+
+const CartLineItemPrice = styled.p`
+    margin: 0;
+    font-size: 2rem;
+`
+
+const RemoveFromCart = styled(FontAwesomeIcon)`
+    cursor: pointer;
+    transition: all 300ms;
+
+    &:hover {
+        color: ${colors.red};
+    }
 `
 
 const Checkout = styled.a`
@@ -50,26 +87,44 @@ const Cart = () => {
     }
 
     return (
-        <StyledCart>
-            <h1>Cart</h1>
-            {cartContext.cart.lineItems.map(item => {
-                return (
-                    <CartLineItem key={item.id}>
-                        <CartItemImage src={item.variant.image.src}/>
+        <Container>
+            <StyledCart>
+                <h1>Cart</h1>
 
-                        <div>
-                            <p>{item.title}</p>
-                            <p>{item.variant.title}</p>
-                        </div>
+                <CartLineItems>
+                    {cartContext.cart.lineItems.map(item => {
+                        return (
+                            <CartLineItem key={item.id}>
+                                <CartLineItemImage
+                                    src={item.variant.image.src}
+                                />
 
-                        <p>{item.variant.price}</p>
+                                <div>
+                                    <CartLineItemTitle>
+                                        {item.title}
+                                    </CartLineItemTitle>
+                                    <CartLineItemVariant>
+                                        {item.variant.title}
+                                    </CartLineItemVariant>
+                                </div>
 
-                        <Remove onClick={() => onClick(item.id)}>X</Remove>
-                    </CartLineItem>
-                )
-            })}
-            <Checkout href={cartContext.cart.webUrl}>Checkout</Checkout>
-        </StyledCart>
+                                <CartLineItemPrice>
+                                    {displayPrice(item.variant.price)}
+                                </CartLineItemPrice>
+
+                                <RemoveFromCart
+                                    icon={faTimesCircle}
+                                    size="2x"
+                                    onClick={() => onClick(item.id)}
+                                />
+                            </CartLineItem>
+                        )
+                    })}
+                </CartLineItems>
+
+                <Checkout href={cartContext.cart.webUrl}>Checkout</Checkout>
+            </StyledCart>
+        </Container>
     )
 }
 
