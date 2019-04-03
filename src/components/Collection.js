90d3c410b2dd78ee1066253ca481e451249c5ce7
Brad Garropy <bradgarropy@gmail.com>
2019-04-03 14:44:14 -0500

    index components.

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,22 +1,58 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Img from "gatsby-image"
-import ProductCategoryHeader from "../styles/ProductCategoryHeader"
-import ProductGrid from "../styles/ProductGrid"
-import ProductPreview from "../styles/ProductPreview"
+import styled from "styled-components"
 import {displayPrice} from "../utils/price"
+
+const CollectionHeader = styled.div`
+    margin-bottom: 3rem;
+
+    h1 {
+        text-align: center;
+        text-transform: uppercase;
+        margin: 0 0 2rem 0;
+    }
+`
+
+const ProductGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 5rem;
+    row-gap: 5rem;
+    align-items: end;
+
+    @media (max-width: 1100px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (max-width: 750px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`
+
+const Product = styled.div`
+    h2 {
+        margin: 1rem 0 0.5rem 0;
+    }
+
+    p {
+        margin: 0;
+        color: ${({theme}) => theme.colors.grey};
+    }
+`
 
 const Collection = ({collection}) => {
     const title = collection.title
-    const image = collection.image.localFile.childImageSharp.fluid
     const products = collection.products
+    const image =
+        collection.image && collection.image.localFile.childImageSharp.fluid
 
     return (
         <>
-            <ProductCategoryHeader>
+            <CollectionHeader>
                 <h1>{title}</h1>
-                <Img fluid={image}/>
-            </ProductCategoryHeader>
+                {image && <Img fluid={image}/>}
+            </CollectionHeader>
 
             <ProductGrid>
                 {products.map(product => {
@@ -31,7 +67,7 @@ const Collection = ({collection}) => {
                     const link = `/${type}/${handle}`
 
                     return (
-                        <ProductPreview key={id}>
+                        <Product key={id}>
                             <a href={link}>
                                 <Img fluid={image}/>
                             </a>
@@ -41,7 +77,7 @@ const Collection = ({collection}) => {
                             </a>
 
                             <p>{displayPrice(price)}</p>
-                        </ProductPreview>
+                        </Product>
                     )
                 })}
             </ProductGrid>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
 import {graphql} from "gatsby"
-import Img from "gatsby-image"
 import Carousel from "../components/Carousel"
+import Collection from "../components/Collection"
 import Container from "../styles/Container"
-import ProductCategoryHeader from "../styles/ProductCategoryHeader"
-import ProductGrid from "../styles/ProductGrid"
-import ProductPreview from "../styles/ProductPreview"
-import {displayPrice} from "../utils/price"
 
 const IndexPage = ({data}) => {
     const carousel = data.allContentfulCarousel.edges[0].node
     const images = carousel.images.map(image => `https:${image.fluid.src}`)
-    const {title, products} = data.shopifyCollection
+    const collection = data.shopifyCollection
 
     return (
         <>
             <Carousel images={images}/>
 
             <Container>
-                <ProductCategoryHeader>
-                    <h1>{title}</h1>
-                </ProductCategoryHeader>
-
-                <ProductGrid>
-                    {products.map(product => {
-                        const id = product.shopifyId
-                        const type = product.productType.toLowerCase()
-                        const handle = product.handle
-                        const title = product.title
-                        const price = product.priceRange.minVariantPrice.amount
-                        const image =
-                            product.images[0].localFile.childImageSharp.fluid
-
-                        const link = `/${type}/${handle}`
-
-                        return (
-                            <ProductPreview key={id}>
-                                <a href={link}>
-                                    <Img fluid={image}/>
-                                </a>
-
-                                <a href={link}>
-                                    <h2>{title}</h2>
-                                </a>
-
-                                <p>{displayPrice(price)}</p>
-                            </ProductPreview>
-                        )
-                    })}
-                </ProductGrid>
+                <Collection collection={collection}/>
             </Container>
         </>
     )
