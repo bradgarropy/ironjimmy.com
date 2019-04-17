a688635ab1d4971008bee9e8afddc84834208d1b
Brad Garropy <bradgarropy@gmail.com>
2019-04-16 22:10:30 -0500

    carousel image responsive sizes. closes #60.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,13 +4,29 @@ import styled from "styled-components"
 
 const StyledCarousel = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 80vh;
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
     transition-property: background-image;
     transition-duration: 1s;
     transition-timing-function: linear;
+
+    @media (max-width: 1200px) {
+        height: 65vh;
+    }
+
+    @media (max-width: 1000px) {
+        height: 50vh;
+    }
+
+    @media (max-width: 800px) {
+        height: 35vh;
+    }
+
+    @media (max-width: 600px) {
+        height: 25vh;
+    }
 `
 
 const Carousel = ({images}) => {
